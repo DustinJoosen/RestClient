078fc0a1e25624f815df493e46dbfe0866dd4ac9
Dustin Joosen <67952109+DustinJoosen@users.noreply.github.com>
2022-06-06 12:34:26 +0200

    Merged PR #1: Setup projects
    * Prepare docs
    
    * Set up projects

---
+++ src/RestClient.Website/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestClient.Website.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
