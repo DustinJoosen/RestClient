108e78065a8eafd70313868b0d3412bcd6028308
DustinJoosen <dustinjoosen2003@gmail.com>
2022-06-15 13:26:41 +0200

    API crud

--- src/RestClient.ORM/Repositories/ApiRepository.cs
+++ src/RestClient.ORM/Repositories/ApiRepository.cs
@@ -1,4 +1,5 @@
-﻿using RestClient.Orm;
+﻿using Microsoft.EntityFrameworkCore;
+using RestClient.Orm;
 using RestClient.Orm.Models;
 using System;
 using System.Collections.Generic;
@@ -10,8 +11,24 @@ namespace RestClient.ORM.Repositories
 {
     public class ApiRepository : BaseRepository<Api>
     {
+        private ApplicationDbContext _context;
         public ApiRepository(ApplicationDbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public override async Task<Api> GetById(Guid id)
+        {
+            return await _context.Api
+                .Include(a => a.Endpoints)
+                .SingleOrDefaultAsync(a => a.Id == id);
+        }
+
+        public override async Task<List<Api>> GetAll()
+        {
+            return await _context.Api
+                .Include(a => a.Endpoints)
+                .ToListAsync();
         }
     }
 }
---
+++ src/RestClient.Website/Controllers/ApiController.cs
@@ -0,0 +1,86 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RestClient.Infra.Dtos;
+using RestClient.Orm;
+
+namespace RestClient.Website.Controllers
+{
+    public class ApiController : Controller
+    {
+        private DataProvider _provider;
+        public ApiController(DataProvider provider)
+        {
+            _provider = provider;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var apis = await _provider.ApiService.GetAll();
+            return View(apis);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ApiDto api)
+        {
+            //validation
+            await _provider.ApiService.Create(api);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var api = await _provider.ApiService.GetById(id);
+            var endpoints = await _provider.EndpointService.GetAll();
+
+            if (api == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Endpoints"] = new SelectList(endpoints, "Id", "Name");
+            return View(api);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ApiDto api)
+        {
+            //validation
+            await _provider.ApiService.Update(api.Id, api);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> RemoveEndpoint(Guid apiid, Guid endpointid)
+        {
+            await _provider.EndpointService.Delete(endpointid);
+            return RedirectToAction(nameof(Edit), new { id = apiid });
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var api = await _provider.ApiService.GetById(id);
+            if (api == null)
+            {
+                return NotFound();
+            }
+            
+            return View(api);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var api = await _provider.ApiService.GetById(id);
+            if (api == null)
+            {
+                return NotFound();
+            }
+
+            await _provider.ApiService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
